d300a1f4fbea85b2accc7cbea808fb38eafac1a2
ToukamSR <paul-henry.ngankam@2025.ucac-icam.com>
2022-05-16 19:12:33 +0100

    Done

--- app.js
+++ app.js
@@ -3,13 +3,28 @@ const adviceId = document.getElementById("advice-id");
 const adviceText = document.getElementById("advice-text");
 const generateBtn = document.getElementById("generate-btn");
 
+function startLoadingAnimation() {
+    generateBtn.style.animationName = "spin";
+}
+
+function stopLoadingAnimation() {
+    generateBtn.style.animationName = "ssd";
+}
+
 // Send Http request
 function request(url, callback) {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", url, true);
 
-  xhr.onload = () => {
-    callback(JSON.parse(xhr.response));
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        setTimeout(() => {
+            callback(JSON.parse(xhr.response));
+            stopLoadingAnimation();
+            generateBtn.style.display = "block";
+            // generateBtn.style.transform = "scaleY(1)";
+        }, 1000);
+    }
   };
 
   xhr.send();
@@ -23,6 +38,8 @@ function generateAdvice() {
     adviceId.innerHTML = response.slip.id;
     adviceText.innerHTML = response.slip.advice;
   });
+
+  startLoadingAnimation();
 }
 
 generateAdvice();
